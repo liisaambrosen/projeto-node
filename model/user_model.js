a878c44d854fbd02f97a59bd0ccc6b427871142b
liisaambrosen <liisa.ambrosen@gmail.com>
2022-04-27 17:45:54 -0300

    base do service

--- model/user_model.js
+++ model/user_model.js
@@ -1,17 +1,20 @@
 const createUser = (name, email, password, phoneNumbers) => {
     console.log(name, email, password, phoneNumbers);
+    // lógica de inserção do usuário no arquivo
 };
 
 const signIn = (email, password) => {
     console.log(email, password);
+    // logica de login
 };
 
 const searchUser = (token) => {
     console.log(token);
+    // lógica de busca do usuário no arquivo
 };
 
 module.exports = {
     createUser,
     signIn,
     searchUser,
-}
+};
---
+++ service/user_service.js
@@ -0,0 +1,31 @@
+const model = require('../model/user_model');
+
+const validateNewUser = (email) => {
+    //logica pra verificar se o email existe no arquivo
+};
+
+const validateLogin = (email, password) => {
+    //logica para verificar o usuario e senha passados
+};
+
+const validateSearch = (token, user_id) => {
+    //logica para verificar o token
+};
+
+const createUser = (name, email, password, phoneNumbers) => {
+    // logica que chama o model.createUser
+};
+
+const signIn = (email, password) => {
+    // logica que chama o model.signIn
+}
+
+const searchUser = (token) => {
+    // logica que chama o model.searchUser
+}
+
+module.exports = {
+    createUser,
+    signIn,
+    searchUser,
+};

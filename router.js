081ef75aae89d0e8482400860dbe797c842e34c2
liisaambrosen <liisa.ambrosen@gmail.com>
2022-04-27 17:31:03 -0300

    Adiciona mbase do router

---
+++ router.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/sign_up', 'alguma coisa');
+
+router.post('/sign_in', 'alguma coisa');
+
+router.get('/search', 'alguma coisa');

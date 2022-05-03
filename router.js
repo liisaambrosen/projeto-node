92dfa0008f4c68d58851485756ecefc005d731b7
liisaambrosen <liisa.ambrosen@gmail.com>
2022-05-03 17:31:27 -0300

    adiciona rota generica pra endpoint que não existe

--- router.js
+++ router.js
@@ -8,4 +8,8 @@ router.post('/sign_in', userController.signIn);
 
 router.get('/search', userController.searchUser);
 
+router.all('*', (_req, res) => {
+    res.status(404).json({ mensagem: "Endpoint não encontrado"});
+});
+
 module.exports = router;

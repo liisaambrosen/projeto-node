bef65719f55da699cae0cfebe64f66bb2cbf7c58
liisaambrosen <liisa.ambrosen@gmail.com>
2022-05-16 10:27:01 -0300

    atualiza validateJWT

--- auth/validateJWT.js
+++ auth/validateJWT.js
@@ -1,9 +1,21 @@
+const model = require('../model/userModel');
+
 const validate = async (req, res, next) => {
     const token = req.headers['authentication'];
+    const { id } = req.params;
 
     if (!token) {
         return res.status(401).json({ mensagem: 'Não autorizado' });
     }
+
+    const allUsers = await model.getUsers();
+    const user = allUsers.find((user) => user.id === JSON.parse(id));
+    const editToken = token.split(" ");
+    
+    if (user.token !== editToken[1]) {
+        return res.status(401).json({ mensagem: 'Não autorizado' });    
+    }
+
     next(token);
 };
 

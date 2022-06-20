80f5489956806ba1125286f0cc9f4350afb6ded4
liisaambrosen <liisa.ambrosen@gmail.com>
2022-06-20 17:16:33 -0300

    adiciona regex no email pra cadastro

--- middlewares/validateNewUser.js
+++ middlewares/validateNewUser.js
@@ -2,9 +2,13 @@ const model = require('../model/userModel');
 
 const validateUser = async (req, res, next) => {
   const { email } = req.body;
+  const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+  if (!regex.test(email)) {
+    return res.status(400).json({ "mensagem": "Email inválido" });
+  }
   const currentUsers = await model.getUsers();
   const invalidEmail = currentUsers.find((user) => user.email === email);
-  if (invalidEmail) return res.status(400).json({ "mensagem": "Email já cadastrado"});
+  if (invalidEmail) return res.status(400).json({ "mensagem": "Email já cadastrado" });
   next();
 }
 
